cae5230ee74910c01c7c9362eb7b2923528e837a
Arthur El Malawi <126459352+ArthurElMalawi@users.noreply.github.com>
2025-09-25 14:57:04 +0000

    add heroheader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import HeroHeader from "@/components/sections/HeroHeader";
 
 export default function Home() {
   return (
     <div>
-
+      <HeroHeader />
     </div>
   );
 }
--- src/components/sections/HeroHeader.tsx
+++ src/components/sections/HeroHeader.tsx
@@ -1,2 +1,18 @@
 import React, { useEffect, useState } from "react";
 import styles from "./HeroHeader.module.scss";
+
+function HeroHeader() {
+    const imgUrl = "https://picsum.photos/1500/1000"
+
+    return (
+        <section className={styles.heroHeader}>
+            <div>
+                <h1>This is a title</h1>
+                <h4>This is a subtitle</h4>
+            </div>
+            <img src={imgUrl} alt="hero header image" />
+        </section>
+    );
+}
+
+export default HeroHeader
