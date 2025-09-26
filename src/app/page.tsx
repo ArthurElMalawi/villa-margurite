1ea8b026bdbfb32c864a85d51db7f46416484e68
Arthur El Malawi <126459352+ArthurElMalawi@users.noreply.github.com>
2025-09-26 07:36:13 +0000

    start section 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import HeroHeader from "@/components/sections/HeroHeader";
+import Rooms from "@/components/sections/Rooms";
 
 export default function Home() {
   return (
     <div>
       <HeroHeader />
+      <Rooms />
     </div>
   );
 }
---
+++ src/components/sections/Rooms.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Rooms.module.scss";
+
+function Rooms() {
+
+    return ( 
+        <section id="rooms" className="section background-yellow">
+
+        </section>
+    );
+}
+
+export default Rooms
