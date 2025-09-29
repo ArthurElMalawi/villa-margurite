f8a848e372274ffbb427212fbbbdf372263257ea
Arthur El Malawi <126459352+ArthurElMalawi@users.noreply.github.com>
2025-09-29 14:41:07 +0000

    start livingroom section

--- src/components/sections/LivingRooms.tsx
+++ src/components/sections/LivingRooms.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import styles from "./Rooms.module.scss";
+import styles from "./Livingroom.module.scss";
+import Image from "next/image";
 
 function Livingroom() {
+  let imgs = []
+
+  for(let i=1; i <= 10; i ++){
+    imgs.push("salon-"+ i);
+  }
+
+  console.log(imgs)
+
   return (
-    <section id="livingroom" className="section">
+    <section id="livingroom" className={`section ${styles.livingroom}`}>
       <h2>Le salon</h2>
+      <div className={styles.container}>
+        <p>text</p>
+        <div className={styles.carousel}>
+          {imgs.map((img) => (
+              <Image
+                key={img}
+                src={`/assets/commons/livingroom/${img}.jpeg`}
+                alt={img}
+                width={1920}
+                height={1080}
+              />
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
