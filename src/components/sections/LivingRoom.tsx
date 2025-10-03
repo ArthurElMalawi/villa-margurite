16694336fb25841691aca4b08f99af7e7dd13ba7
ArthurElMalawi <elmalawia@gmail.com>
2025-10-03 11:29:03 +0200

    fix rooms section on mobile

--- src/components/sections/LivingRoom.tsx
+++ src/components/sections/LivingRoom.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./LivingRoom.module.scss";
-import Image from "next/image";
 import Carousel from "@/components/utils/Carousel";
 
 export default function LivingRoom() {
-  const imgs = []
+  const imgs = [];
 
-  for(let i=1; i <= 8; i ++){
-    imgs.push("salon-"+ i);
+  for (let i = 1; i <= 8; i++) {
+    imgs.push("salon-" + i);
   }
 
   return (
@@ -15,20 +14,27 @@ export default function LivingRoom() {
       <h2>Le salon</h2>
       <div className={styles.container}>
         <p>
-          Le salon de la résidence est un espace lumineux et accueillant, ouvert sur le jardin grâce à ses grandes baies vitrées. Ses canapés confortables, ses tables basses et ses plantes en font un lieu idéal pour se détendre, travailler ou partager un moment convivial entre colocataires.
-          <br/>
-          <br/>
-          Avec son parquet en bois, ses moulures élégantes et sa décoration moderne, ce grand salon allie charme de l’ancien et confort contemporain. Véritable cœur de la maison, il invite chacun à s’y sentir rapidement chez soi.
+          Le salon de la résidence est un espace lumineux et accueillant, ouvert
+          sur le jardin grâce à ses grandes baies vitrées. Ses canapés
+          confortables, ses tables basses et ses plantes en font un lieu idéal
+          pour se détendre, travailler ou partager un moment convivial entre
+          colocataires.
+          <br />
+          <br />
+          Avec son parquet en bois, ses moulures élégantes et sa décoration
+          moderne, ce grand salon allie charme de l’ancien et confort
+          contemporain. Véritable cœur de la maison, il invite chacun à s’y
+          sentir rapidement chez soi.
         </p>
-        <Carousel carouselItems={ 
-          imgs.map((img) => (
-              <Image
-                key={img}
-                src={`/assets/commons/livingroom/${img}.jpeg`}
-                alt={img}
-                width={1920}
-                height={1080}
-              />
+        <Carousel
+          carouselItems={imgs.map((img) => (
+            <img
+              key={img}
+              src={`/assets/commons/livingroom/${img}.jpeg`}
+              alt={img}
+              width={1920}
+              height={1080}
+            />
           ))}
         />
       </div>
